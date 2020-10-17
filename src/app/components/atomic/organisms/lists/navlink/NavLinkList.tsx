f2fc558ce4e987f053c64ff701efc5b8cb448682
Robin Scholtes <robin@theweirdscience.com>
2020-10-17 22:36:59 +1300

    select component with option childrenm added some lists ( some menu items now listed in menu as well )

---
+++ src/app/components/atomic/organisms/lists/navlink/NavLinkList.tsx
@@ -0,0 +1,22 @@
+import React, { FC } from 'react';
+import NavLinkLi from "../../../molecules/list-items/nav-link/NavLinkLi";
+
+export interface NavLinkListChild {
+    id: string,
+    title: string,
+    to: string
+}
+
+interface NavLinkListProps {
+    className: string,
+    children: NavLinkListChild[]
+}
+
+const NavLinkList: FC<NavLinkListProps> = ( { className, children }) =>
+    <ul className={ className }>
+        { children.map((child) => (
+            <NavLinkLi to={ child.to } id={ child.id } title={ child.title }/>
+        ))}
+    </ul>;
+
+export default NavLinkList;
