e51d37c95fdb72a1ab6335cb1ef26576a3f35701
Robin Scholtes <robin@theweirdscience.com>
2020-10-21 21:29:39 +1300

    some profile page additions

--- src/app/components/sections/profile/Profile.tsx
+++ src/app/components/sections/profile/Profile.tsx
@@ -1,23 +1,20 @@
 import React, { FC } from 'react';
 import Section from '../../layout/section/Section';
 import Content from '../../layout/content/Content';
+import UserDetails from '../../atomic/molecules/user-details/UserDetails';
 
 const Profile: FC = () => (
-  <Section id="profile" title="Username" themeClass="theme-tertiary">
+  <Section id="profile" themeClass="theme-tertiary">
     <Content colSize="column">
-      <img src="/assets/images/dolphin.png" alt="user" title="user" className="user-image" />
-      <ul className="btn-group horizontal">
-        <li>
-          <button className="btn btn-primary btn-compact" type="button">
-            Change image
-          </button>
-        </li>
-        <li>
-          <button className="btn btn-primary btn-compact" type="button">
-            Delete image
-          </button>
-        </li>
-      </ul>
+      <UserDetails
+        id="1"
+        imgSrc="/assets/images/dolphin.png"
+        username="Empress"
+        email="dolphins@sea.co.nz"
+        rank="Asteroid"
+        sightings="3"
+        region="Wellington"
+      />
     </Content>
   </Section>
 );
